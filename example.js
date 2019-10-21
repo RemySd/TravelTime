ef4ac9cd58c29d8c78dbd4909604777497766bce
Remy <remy.sainsard@gmail.com>
2019-10-21 20:14:44 +0200

    lib TravelTime calculate

---
+++ example.js
@@ -0,0 +1,15 @@
+import * as travelTime from "./index.js";
+
+let t1 = {
+    hours: 2,
+    minutes: 25,
+    secondes: 50
+};
+
+let t2 = {
+    hours: 3,
+    minutes: 22,
+    secondes: 33
+};
+
+console.log(travelTime.calculate(t1, t2));
